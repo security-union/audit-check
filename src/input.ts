7c41b903c6c89485af03c696423158b6a932cc75
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2022-11-22 00:22:37 -0500

    V5 (#5)
    * another test
    
    * compile github action
    
    * save
    
    Co-authored-by: Dario Lencina <dario@securityunion.dev>

--- src/input.ts
+++ src/input.ts
@@ -7,10 +7,12 @@ import { input } from '@actions-rs/core';
 // Parsed action input
 export interface Input {
     token: string;
+    cargoLockPath: string | undefined;
 }
 
 export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
+        cargoLockPath: input.getInput('cargo-lock-path', {required: false}),
     };
 }
